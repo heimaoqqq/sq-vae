186de630a01c2ae067da583aa662e511e1d212a2
heimaoqqq <heimaoqqq@example.com>
2025-07-09 16:42:02 +0800

    增强模型：1) 增加重建损失权重以改善图像清晰度 2) 添加Swish激活函数支持 3) 添加通道乘数以增加网络容量 4) 创建增强型和深度网络配置文件

--- vision/networks/microdoppler.py
+++ vision/networks/microdoppler.py
@@ -1,13 +1,50 @@
 from networks.net_64 import EncoderVqResnet64, DecoderVqResnet64
+import torch.nn as nn
+from networks.util import ResBlock, get_activation
 
 
 class EncoderVq_resnet(EncoderVqResnet64):
     def __init__(self, dim_z, cfgs, flg_bn, flg_var_q):
-        super(EncoderVq_resnet, self).__init__(dim_z, cfgs, flg_bn, flg_var_q)
+        # 获取通道乘数
+        channel_multiplier = getattr(cfgs.network, "channel_multiplier", 1.0)
+        adjusted_dim_z = int(dim_z * channel_multiplier)
+        
+        # 获取激活函数
+        self.activation = getattr(cfgs.network, "activation", "relu")
+        
+        super(EncoderVq_resnet, self).__init__(adjusted_dim_z, cfgs, flg_bn, flg_var_q)
         self.dataset = "MicroDoppler"
+        
+        # 如果指定了自定义激活函数，重新构建ResBlock
+        if self.activation != "relu":
+            # 重新构建ResBlock部分
+            num_rb = cfgs.network.num_rb
+            layers_resblocks = []
+            for i in range(num_rb-1):
+                layers_resblocks.append(ResBlock(adjusted_dim_z, activation=self.activation))
+            self.res = nn.Sequential(*layers_resblocks)
+            self.res_m = ResBlock(adjusted_dim_z, activation=self.activation)
+            if self.flg_variance:
+                self.res_v = ResBlock(adjusted_dim_z, activation=self.activation)
 
 
 class DecoderVq_resnet(DecoderVqResnet64):
     def __init__(self, dim_z, cfgs, flg_bn):
-        super(DecoderVq_resnet, self).__init__(dim_z, cfgs, flg_bn)
-        self.dataset = "MicroDoppler" 
+        # 获取通道乘数
+        channel_multiplier = getattr(cfgs.network, "channel_multiplier", 1.0)
+        adjusted_dim_z = int(dim_z * channel_multiplier)
+        
+        # 获取激活函数
+        self.activation = getattr(cfgs.network, "activation", "relu")
+        
+        super(DecoderVq_resnet, self).__init__(adjusted_dim_z, cfgs, flg_bn)
+        self.dataset = "MicroDoppler"
+        
+        # 如果指定了自定义激活函数，重新构建ResBlock
+        if self.activation != "relu":
+            # 重新构建ResBlock部分
+            num_rb = cfgs.network.num_rb
+            layers_resblocks = []
+            for i in range(num_rb-1):
+                layers_resblocks.append(ResBlock(adjusted_dim_z, activation=self.activation))
+            self.res = nn.Sequential(*layers_resblocks) 
--- vision/networks/util.py
+++ vision/networks/util.py
@@ -1,21 +1,41 @@
-from torch import nn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-## Resblocks
+
+class Swish(nn.Module):
+    """Swish激活函数 x * sigmoid(x)"""
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+def get_activation(name):
+    """获取激活函数"""
+    if name.lower() == "relu":
+        return nn.ReLU(inplace=True)
+    elif name.lower() == "leakyrelu":
+        return nn.LeakyReLU(0.2, inplace=True)
+    elif name.lower() == "swish":
+        return Swish()
+    elif name.lower() == "sigmoid":
+        return nn.Sigmoid()
+    elif name.lower() == "tanh":
+        return nn.Tanh()
+    else:
+        return nn.ReLU(inplace=True)  # 默认使用ReLU
+
+
 class ResBlock(nn.Module):
-    def __init__(self, dim, act="relu"):
-        super().__init__()
-        if act == "relu":
-            activation = nn.ReLU()
-        elif act == "elu":
-            activation = nn.ELU()
-        self.block = nn.Sequential(
-            activation,
-            nn.Conv2d(dim, dim, 3, 1, 1),
+    def __init__(self, dim, activation="relu"):
+        super(ResBlock, self).__init__()
+        self.res = nn.Sequential(
+            nn.Conv2d(dim, dim, 3, stride=1, padding=1),
             nn.BatchNorm2d(dim),
-            activation,
-            nn.Conv2d(dim, dim, 1),
+            get_activation(activation),
+            nn.Conv2d(dim, dim, 1, stride=1, padding=0),
             nn.BatchNorm2d(dim)
         )
+        self.activation = get_activation(activation)
 
     def forward(self, x):
-        return x + self.block(x)
+        return self.activation(x + self.res(x))
